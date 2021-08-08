6116d218d587640944925cc68a234d972ddb3427
Anonymous <>
2021-08-09 02:17:09 +0900

    :3

--- preloads/reservation.ts
+++ preloads/reservation.ts
@@ -11,8 +11,6 @@ let checkExist = setInterval(() => {
 }, 500);
 
 ipcRenderer.on('here-is-info', (evt, payload)=> {
-    console.log(payload[0]);
-    console.log("hello");
     let name = document.getElementById('nametd') as HTMLTableHeaderCellElement
     let vaccine = document.getElementById('vaccinetd') as HTMLTableDataCellElement
     name!.innerText = payload[0]
@@ -23,7 +21,7 @@ ipcRenderer.on('here-is-info', (evt, payload)=> {
 
 ipcRenderer.on("console", (_, msg) => {
     let consolee = document.getElementById('bottomdiv') as HTMLDivElement
-    if (consolee.children.length == 100000)
+    if (consolee.innerHTML.length > 45000)
         consolee.innerHTML = "";
     consolee.innerHTML += msg + "\n"
     consolee.scrollTop = consolee.scrollHeight;
